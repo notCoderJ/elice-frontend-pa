90d7f4fcbe0dd6f75b8478490175e14026d0c7f7
Jinseong Jeon <aimer120@nate.com>
2022-02-07 10:36:13 +0900

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ const API_BASE_URL = 'https://api-rest.elice.io/org/academy';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 3000,
+  timeout: 10000, // 시간초과로 10초로 변경.
   headers: {
     'Content-Type': 'application/json',
   },
